0897d5bf1681fc23b58bae117f94d3e88a9657a2
Liz Emerson <uknity@gmail.com>
2021-07-04 23:25:15 -0400

    corrected navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -18,43 +18,110 @@ function Header() {
 			></div>
 
 			<div className="row" id="nameRow">
-				<div className="col-9 col-md-4">
-					<div className="bg-light display-1 p-0" id="ee">
-						Elizabeth Emerson
-					</div>
-					<div className="bg-light" id="subTitle">
-						Grow, build, learn, repeat
-					</div>
+				{/* <div className="col-9 col-md-4"> */}
+				<div className="bg-light display-1 p-0" id="ee">
+					Elizabeth Emerson
 				</div>
-				<div className="col-3 col-md-8 bg-light">
-					<Navbar.Toggle aria-controls="responsive-navbar-nav" />
-					<Navbar.Collapse id="responsive-navbar-nav">
-						<Nav>
-							<Nav.Link href="/about">About</Nav.Link>
-							<Nav.Link href="/projects">Projects</Nav.Link>
-							<Nav.Link href="/contact">Contact</Nav.Link>
-							<Nav.Link
+			</div>
+
+			<nav
+				className="navbar navbar-expand-lg navbar-light bg-light"
+				id="subtitleLinkRow"
+			>
+				<div className="navbar-brand bg-light" id="subTitle">
+					Grow, build, learn, repeat
+				</div>
+
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-toggle="collapse"
+					data-target="#navbarTogglerDemo01"
+					aria-controls="navbarTogglerDemo01"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" >
+					
+					<ul className="nav  mt-2 mt-lg-0 ms-auto">
+						{/* <ul className="navbar-nav  mb-2 mb-lg-0" id="linkRow"> */}
+						
+						<li className="nav-item ">
+							<Link
+								to="/about"
+								id="link"
+								className={
+									window.location.pathname === "/about"
+										? "nav-link active"
+										: "nav-link"
+								}
+							>
+								About Me{" "}
+							</Link>
+						</li>
+						<li className="nav-item ">
+							<Link
+								to="/projects"
+								id="link"
+								className={
+									window.location.pathname === "/projects"
+										? "nav-link active"
+										: "nav-link"
+								}
+							>
+								Projects{" "}
+							</Link>
+						</li>
+						<li className="nav-item ">
+							<Link
+								to="/contact"
+								id="link"
+								className={
+									window.location.pathname === "/contact"
+										? "nav-link active"
+										: "nav-link"
+								}
+							>
+								Contact Me{" "}
+							</Link>
+						</li>
+						<li className="nav-item ">
+							<a
+								className="nav-link"
+								id="link"
 								rel="noopener noreferrer"
 								href="https://docs.google.com/document/d/1XnnyCAyvonrlKVCC-sQ2VxHgFOKzGe6oD_FBRDAoBKk/edit?usp=sharing"
 								target="_blank"
 								download
 							>
-								Resume
-							</Nav.Link>
-							<Nav.Link href="/">Home</Nav.Link>
-						</Nav>
-					</Navbar.Collapse>
+								Resume{" "}
+							</a>
+						</li>
+						
+						<li className="nav-item" >
+							<Link
+								to="/"
+								id="link"
+								className={
+									window.location.pathname === "/"
+										? "nav-link active"
+										: "nav-link"
+								}
+							>
+								Home{" "}
+							</Link>
+						</li>
+					</ul>
 				</div>
-			</div>
+			</nav>
 
-			{/* <nav className="navbar  navbar-light bg-light" id="subtitleLinkRow">
-				<div className="navbar-brand bg-light" id="subTitle">
-					Grow, build, learn, repeat
-				</div>
-				
-			</nav> */}
-		</div>
-	);
+		
+  </div>
+  	);
 }
 
 export default Header;
+
+		
